6b85924e5c36d0916889d77e6be5ec7bdbe791c0
Bryan Granado <granadobryan1@gmail.com>
2021-12-27 23:38:31 -0400

    cargando registro s en activities

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -7,26 +7,26 @@ export const Table = ({props}) => {
             <table className='table-auto w-full'>
                 <thead>
                     <tr>
-                    <th className='text-gray-500 font-mono text-left'>City</th>
-                    <th className='text-gray-500 font-mono text-left'>Artist</th>
-                    <th className='text-gray-500 font-mono text-left'>Year</th>
+                    <th className='text-gray-500 font-mono text-left'>AMOUNT</th>
+                    <th className='text-gray-500 font-mono text-left'>TOKEN TRANSACTION</th>
+                    <th className='text-gray-500 font-mono text-left'>DATE</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
+                    <td className='text-gray-400'>0.00256397 eth</td>
+                    <td className='text-meta-blue'>2ywodmdfp243534534sdfklfd</td>
+                    <td>27-10-2021</td>
                     </tr>
                     <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
+                    <td className='text-gray-400'>0.09563977 eth</td>
+                    <td className='text-meta-blue'>2ywodmdfp243534534sdfklfd</td>
+                    <td>27-10-2021</td>
                     </tr>
                     <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
+                    <td className='text-gray-400'>0.00147563 eth</td>
+                    <td className='text-meta-blue'>2ywodmdfp243534534sdfklfd</td>
+                    <td>27-10-2021</td>
                     </tr>
                 </tbody>
             </table>
